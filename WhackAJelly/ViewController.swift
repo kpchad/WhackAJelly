d683a7d6cdc931857e46694d5044220ad65c4de8
Kyle Chadwick <kylepchad@gmail.com>
2017-10-12 21:59:17 -0700

    hit test

---
+++ WhackAJelly/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  WhackAJelly
+//
+//  Created by Kyle Chadwick on 10/12/17.
+//  Copyright © 2017 Kyle Chadwick. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configureation = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configureation)
+        let tapGeuestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGeuestureRecognizer)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
+    func addNode(){
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.position = SCNVector3(0,0,-1)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            print("didn't touch anything")
+        } else {
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry!)
+        }
+    }
+    
+}
+
